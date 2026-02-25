bd37ff29d4ddc46325e21653d170c7b1a7f2b119
Jam <JWalkerdine21@hotmail.com>
2026-02-25 19:03:11 +0000

    Add trace utilities

--- lib/examples/e_AL.ex
+++ lib/examples/e_AL.ex
@@ -127,8 +127,8 @@ defmodule Examples.AL do
   end
 
   example total_failure_aborts_transaction() do
-    {:aborted, :failure} = AL.eval([:fail])
-    {:aborted, :failure} = AL.eval([{:get_class, :nonexistent_object_xyz, :"$x"}])
+    {:aborted, _trace} = AL.eval([:fail])
+    {:aborted, _trace} = AL.eval([{:get_class, :nonexistent_object_xyz, :"$x"}])
     :ok
   end
 
