32c25819cdb89c816994e716b7c54a62d1a2914a
l4e21 <jwalkerdine21@hotmail.com>
2026-06-29 17:19:36 +0100

    Generalise cell and propagator views into joined constraint view

--- lib/AL/views.ex
+++ lib/AL/views.ex
@@ -18,75 +18,27 @@ defmodule AL.Views do
     |> Mondrian.title("Examine")
   end
 
-  defview cell_view(self = %AL.Object{}, builder) do
+  defview constraint_view(self = %AL.Object{}, builder) do
     result =
       AL.run do
-        class(^self.id, :cell)
-        get_slot(^self.id, :subscribers, subscribers)
-        get_slot(^self.id, :value, value)
-        unify(nodes, [^self.id | subscribers])
-        unify(children, %{^self.id => subscribers})
+        dependents(^self.id, dependents)
       end
 
     case result do
       {:atomic, {bindings, _program_state}} ->
+        dependents = Map.get(bindings, :"$dependents")
+
         builder.mondrian()
-        |> Mondrian.title("Cell View")
+        |> Mondrian.title("Constraint Graph")
         |> Mondrian.nodes(
-          Enum.map(
-            Map.get(bindings, :"$nodes"),
-            fn x -> %AL.Object{id: x} end
-          )
+          dependents
+          |> Map.keys()
+          |> Enum.map(fn x -> %AL.Object{id: x} end)
         )
         |> Mondrian.node_label(fn node -> Atom.to_string(node.id) end)
         |> Mondrian.edges(fn node ->
-          case Map.get(Map.get(bindings, :"$children"), node.id) do
-            nil -> []
-            children -> Enum.map(children, fn c -> %AL.Object{id: c} end)
-          end
-        end)
-        |> Mondrian.layout(:horizontal_tree)
-
-      _e ->
-        builder.empty()
-    end
-  end
-
-  defview propagator_view(self = %AL.Object{}, builder) do
-    result =
-      AL.run do
-        class(^self.id, :propagator)
-        get_slot(^self.id, :input_cells, input_cells)
-        get_slot(^self.id, :output_cell, output_cell)
-        unify(nodes, [^self.id | [output_cell | input_cells]])
-
-        fold_left(
-          input_cells,
-          [acc, h, result],
-          [put(acc, h, [^self.id], result)],
-          %{^self.id => [output_cell]},
-          children
-        )
-      end
-
-    IO.inspect(result)
-
-    case result do
-      {:atomic, {bindings, _program_state}} ->
-        builder.mondrian()
-        |> Mondrian.title("Propagator View")
-        |> Mondrian.nodes(
-          Enum.map(
-            Map.get(bindings, :"$nodes"),
-            fn x -> %AL.Object{id: x} end
-          )
-        )
-        |> Mondrian.node_label(fn node -> Atom.to_string(node.id) end)
-        |> Mondrian.edges(fn node ->
-          case Map.get(Map.get(bindings, :"$children"), node.id) do
-            nil -> []
-            children -> Enum.map(children, fn c -> %AL.Object{id: c} end)
-          end
+          Map.get(dependents, node.id)
+          |> Enum.map(fn c -> %AL.Object{id: c} end)
         end)
         |> Mondrian.layout(:horizontal_tree)
 
