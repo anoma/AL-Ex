4bff711b06340f886fe09d0b4ccf71ea006e25ff
l4e21 <jwalkerdine21@hotmail.com>
2026-09-21 18:13:42 +0100

    Release 0.4.2

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule AL.MixProject do
   def project do
     [
       app: :al,
-      version: "0.4.1",
+      version: "0.4.2",
       elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
